bb6a0a1b218663f7bca535ebac0d5f2b673464ce
hgranule <wp3d3p@gmail.com>
2020-02-16 05:17:30 +0300

    INIT COMMIT

---
+++ intra_main.py
@@ -0,0 +1,22 @@
+import intra_data as fr_42
+import intra_parser as fr_42
+
+# _UID = 'CLIENT_ID'
+# _SECRET = 'SECRET'
+
+# fr_42.configure(_UID, _SECRET)
+
+# BIG_DATA = []
+
+# BIG_DATA += fr_42.import_users_json('2019_june_raw.json')
+# BIG_DATA += fr_42.import_users_json('2019_july_raw.json')
+# BIG_DATA += fr_42.import_users_json('2019_august_raw.json')
+
+# WIN_DATA = []
+# LOS_DATA = []
+
+# for dat in BIG_DATA:
+# 	if len(dat['cursus_users']) > 1:
+# 		WIN_DATA.append(dat)
+# 	else:
+# 		LOS_DATA.append(dat)
